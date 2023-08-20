fa5569db1d28335c8d07f60a458f4903f01654f7
Xiangchong Li <mr.superonion@hotmail.com>
2023-08-20 04:28:00 -0400

    update

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 800;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=12"
@@ -17,7 +17,7 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 8;
-static unsigned int columns    = 2;
+static unsigned int columns    = 1;
 
 /*
  * Characters not considered part of a word while deleting words
